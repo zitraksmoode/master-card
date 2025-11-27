0a74cf50c8dd5873a58878c1d280b5cd1b7a774c
zitraksmoode <Cybindmitrij711@gmail.com>
2025-11-27 20:05:47 +0500

    init: spring boot project from start.spring.io

---
+++ src/main/java/Bank/master_card/MasterCardApplication.java
@@ -0,0 +1,13 @@
+package Bank.master_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Bank/master_card/MasterCardApplicationTests.java
@@ -0,0 +1,13 @@
+package Bank.master_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
